1571f57f192f0d32e44f55740114287fde4091c7
Phillip <schommarzp@gmail.com>
2022-05-07 18:23:23 +0200

    More chagne port

--- index.js
+++ index.js
@@ -2,11 +2,10 @@
 const express = require('express')
 const Gun = require('gun');
 const app = express()
-const port = 8765
+const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765;
 app.use(Gun.serve);
+app.use(express.static(__dirname));
 
-const server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const server = app.listen(port)
 
 Gun({ web: server });
