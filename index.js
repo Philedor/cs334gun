53ba5f07bfe27b2dbf7eb968d8cba4c4c0faa920
Phillip <schommarzp@gmail.com>
2022-05-08 09:52:31 +0200

    Stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 const express = require('express')
-const Gun = require('gun');
+const Gun = require('gun')
 const app = express()
 const port = process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765;
 app.use(Gun.serve);
